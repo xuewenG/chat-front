f1f15f9796be9fbc3be990a0224072141d7133a6
xuewenG <gexuewen@outlook.com>
2021-05-06 23:56:02 +0800

    feat: change screen share

--- src/main/window/screenShareWindow.ts
+++ src/main/window/screenShareWindow.ts
@@ -1,12 +1,10 @@
 import { WINDOW_NAME } from '@common/window/windowName'
-import { createWindow, getWindowByName } from '.'
+import { createWindow } from '.'
 
 const createScreenShareWindow = () => {
-  const mainWin = getWindowByName(WINDOW_NAME.MAIN_WINDOW)
   createWindow(WINDOW_NAME.SCREEN_SHARE_WINDOW, '/screen', {
-    width: 1632,
-    height: 918,
-    parent: mainWin,
+    width: 720,
+    height: 510,
   })
 }
 
--- src/render/views/screen/Index.vue
+++ src/render/views/screen/Index.vue
@@ -9,18 +9,22 @@
     <div class="op-bar">
       <img
         class="op-item mini"
-        src="../../assets/mini-gray.svg"
+        src="../../assets/mini.svg"
         alt="最小化"
         @click="handleHideWindow"
       />
       <img
         class="op-item close"
-        src="../../assets/close-gray.svg"
+        src="../../assets/close.svg"
         alt="关闭"
         @click="handleCloseWindow"
       />
     </div>
     <video />
+    <div class="tip">正在共享您的屏幕</div>
+    <div class="tool-bar">
+      <div class="stop">停止共享</div>
+    </div>
   </div>
 </template>
 
@@ -56,6 +60,10 @@ export default defineComponent({
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #8a8a8a;
   .op-bar {
     position: fixed;
     top: 0;
@@ -79,8 +87,28 @@ export default defineComponent({
     }
   }
   video {
-    width: 100vw;
-    height: 100vh;
+    width: 75%;
+    height: 75%;
+  }
+  .tip {
+    color: white;
+  }
+  .tool-bar {
+    width: 100%;
+    height: 52px;
+    margin-bottom: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .stop {
+      width: 82px;
+      height: 32px;
+      text-align: center;
+      line-height: 32px;
+      border-radius: 8px;
+      color: white;
+      border: 2px solid #aaa;
+    }
   }
 }
 </style>
